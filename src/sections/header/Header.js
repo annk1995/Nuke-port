a4d6ee3b1376399ae8bd5fdc542585b17a419d0e
annk1995 <kabuikariuki052@gmail.com>
2023-07-10 15:58:06 +0300

    added socilas

--- src/sections/header/Header.js
+++ src/sections/header/Header.js
@@ -1,8 +1,25 @@
 import React from 'react'
-
+import HeaderImage from '../../assets/software.jpg'
 const Header = () => {
   return (
-    <div>Header</div>
+  <header id="header">
+ <div className="container header__container">
+  <div className="header__image">
+    <img src={HeaderImage}alt="Header Potrait" />
+  </div>
+  <h3>Nuke Softwares Kenya</h3>
+  <p>lorem ipsum dolor sit amet, consectetur adip lorem lorem</p>
+  <div className="header__cta">
+    <a href="#contact">Let's Talk</a>
+    <a href="#portfolio">Our Work</a>
+
+
+  </div>
+  <div className="header__socials">
+    {}
+  </div>
+ </div>
+  </header>
   )
 }
 
---
+++ src/sections/header/data.js
@@ -0,0 +1,13 @@
+import {AiOutlineInstagram} from 'react-icons/ai'
+import {AiOutlineTwitter} from 'react-icons/ai'
+
+import {AiFillGithub} from 'react-icons/ai'
+
+
+const data =[
+    {id:1,link: '#',icon:<AiOutlineInstagram/>},
+{id:2,link: '#',icon:<AiOutlineTwitter/>},
+    {id:1,link: '#',icon:<AiFillGithub/>}
+]
+
+export default data
