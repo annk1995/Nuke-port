6839d43a5c017c50612d02f5a271e785659f2102
annk1995 <kabuikariuki052@gmail.com>
2023-07-10 19:19:58 +0300

    added context on header

--- src/sections/header/Header.js
+++ src/sections/header/Header.js
@@ -1,22 +1,26 @@
 import React from 'react'
-import HeaderImage from '../../assets/software.jpg'
+import HeaderImage from '../../assets/business.jpeg'
+import data from './data'
+import './header.css'
 const Header = () => {
   return (
   <header id="header">
- <div className="container header__container">
-  <div className="header__image">
+ <div className=" header__container">
+  <div className="header__profile">
     <img src={HeaderImage}alt="Header Potrait" />
   </div>
   <h3>Nuke Softwares Kenya</h3>
   <p>lorem ipsum dolor sit amet, consectetur adip lorem lorem</p>
   <div className="header__cta">
-    <a href="#contact">Let's Talk</a>
-    <a href="#portfolio">Our Work</a>
+    <a href="#contact" className="btn primary">Let's Talk</a>
+    <a href="#portfolio" className="btn light">Our Work</a>
 
 
   </div>
   <div className="header__socials">
-    {}
+    {
+      data.map(item => <a key={item.id}href={item.link} target="_blank" rel="noopener norefferer" >{item.icon}</a>)
+    }
   </div>
  </div>
   </header>
